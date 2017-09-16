e562e2d94048fd9c9b023298379980c6c18ad893
George Higbie <gehigbie@gmail.com>
2017-09-16 16:15:34 -0600

    Add some coditional statements

---
+++ src/ControlFlow.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>){
+    val a = 2
+    val b = 3
+    val accountBalance = 100
+    val price = 50
+
+    if(a == b){
+        println("Yo! A does equal B")
+    }
+
+    if (a != b){
+        println("Yo mama! A does not equal B")
+    }
+
+    if(price <= accountBalance){
+        println("You can buy this")
+    }else{
+        println("Get a job!")
+    }
+
+}
