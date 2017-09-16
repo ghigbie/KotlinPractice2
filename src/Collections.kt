02e5b6e2f670e971f205c9cc9356aac6c23c8cd4
George Higbie <gehigbie@gmail.com>
2017-09-16 16:55:53 -0600

    Perform operations on array

--- src/Collections.kt
+++ src/Collections.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>){
 
     val imperials = listOf("Emperor", "Darth Vader", "Boba Fett", "Govenor Tarkin", "Kailo Ren" ) //this is an immutable version of the arrray, we can't add t it nor can we change it
+    println("Sorted:  ${imperials.sorted()} ")
+    println("imperials[2]: ${imperials[2]}")
+    println("first: ${imperials.first()}")
+    println("last: ${imperials.last()}")
     println(imperials)
 
 }
