ce27b2d08826019c51ffab7011c22f7894fe24d7
George Higbie <gehigbie@gmail.com>
2017-09-16 16:36:18 -0600

    Add file with when statement

---
+++ src/WhenStatements.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    val x = 1
+
+    when (x) {
+        1 -> println("x = 1")
+        2 -> println("x = 2")
+        3 -> println("x = 3")
+        4 -> println("x = 4")
+        5 -> println("x = 5")
+        else -> println("x is not in range")
+    }
+}
