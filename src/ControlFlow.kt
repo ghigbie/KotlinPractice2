9934207ef4c0342e108ddc840154396d04d57454
George Higbie <gehigbie@gmail.com>
2017-09-16 16:24:04 -0600

    Add more complex conditional statement

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>){
     val b = 3
     val accountBalance = 100
     val price = 50
-    val degrees = 70
+    val degrees = 32
 
     if(a == b){
         println("Yo! A does equal B")
@@ -21,6 +21,12 @@ fun main(args: Array<String>){
 
     if(degrees == 70){
         println("This is some nice weather. It's ${degrees} degrees outside.")
+    }else if(degrees <= 70 && degrees >= 50){
+        println("It's kind of nippy outside. It's ${degrees} degrees outside.")
+    }else if(degrees >= 71){
+        println("It's hot out there. It's ${degrees} degrees outside")
+    }else if(degrees < 49){
+        println("It's so cold you're gonna die. It's ${degrees} degrees outside.")
     }else{
         println("The end is near. It's ${degrees} degrees outside")
     }
