9dd92ab4c57165e068e4fbfb499f53cfb96a61a6
George Higbie <gehigbie@gmail.com>
2017-09-16 17:32:30 -0600

    Add new collection type

--- src/Collections.kt
+++ src/Collections.kt
@@ -12,11 +12,11 @@ fun main(args: Array<String>){
     val rebels = arrayListOf("Leia", "Luke", "Han Solo", "Mon Mothma", "Yoda") //This collection type is mutable
     println("Size: ${rebels.size}")
     println("Add: ${rebels.add("Chebaca")} ${rebels}")
-    println("Add at specific index: ${rebels.add(0, "Lando")} ${rebels}")
+    println("Add at specific index: ${rebels.add(0, "Lando")} $rebels")
     println("Where is Luke: ${rebels.indexOf("Luke")}")
     println("Who is at 3: ${rebels.get(3)}")
     println("Han Solo is not there: ${rebels.remove("Han Solo")}")
-    println("The rebels now have only: ${rebels}")
+    println("The rebels now have only: $rebels")
 
     //the below collection type is immutable
     val rebelVehicles = mapOf("Solo" to "Millenium Falcon", "Luke" to "Landspeeder", "Reg" to "X-wing")
@@ -29,5 +29,16 @@ fun main(args: Array<String>){
     println(rebelVehicles.getOrDefault("Akbar", "This ship does not exist"))
     println("The keys are ${rebelVehicles.keys}")
     println("The values are ${rebelVehicles.values}")
+
+    //the below collection type is mutable
+    val vehicles = hashMapOf("Solo" to "Millenium Falcon", "Luke" to "Landspeeder", "Reg" to "X-wing", "Bobba Fett" to "Rocket Pack")
+    vehicles["Luke"] = "X-wing"
+    vehicles.put("Leiah", "Tantive IV")
+    println("The vehicles are: $vehicles")
+    println("The vehicles only are: ${vehicles.values}")
+    println("The owners of the vehicles are ${vehicles.keys}")
+    vehicles.remove("Bobba Fett")
+    println("The owners of the vehicles are all rebels: ${vehicles.keys}.")
+
 
 }
