b84c9df6055f10eb60711e5ab7c8dfcca7482bbf
George Higbie <gehigbie@gmail.com>
2017-09-16 16:45:06 -0600

    Add jediIsFeeling funciton

--- src/WhenStatements.kt
+++ src/WhenStatements.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>){
-    val x = 1
+    val x = 10
 
     when (x) {
         1 -> println("x = 1")
@@ -9,4 +9,16 @@ fun main(args: Array<String>){
         5 -> println("x = 5")
         else -> println("x is not in range")
     }
+
+    fun jediIsFeeling(jedi: String = "vader", mood: String = "angry"){
+        when (mood){
+            "angry" -> println("${jedi} is feeling ${mood}, so he chokes the person nearby.")
+            "happy" -> println("${jedi} is feeling ${mood}, so he dances for a bit.")
+            "hungry" -> println("${jedi} is feeling ${mood}, so he eats a hamburger.")
+            else -> println("${jedi} just sits there because he is feeling ${mood}.")
+        }
+    }
+
+    jediIsFeeling("Vader", "happy")
+    jediIsFeeling("Obiwon", "hungry")
 }
