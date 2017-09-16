967d8cd31061ac39c4566460a0eea48d98315ed3
George Higbie <gehigbie@gmail.com>
2017-09-16 16:51:59 -0600

    Add one new collection

---
+++ src/Collections.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>){
+
+    val imperials = listOf("Emperor", "Darth Vader", "Boba Fett", "Govenor Tarkin", "Kailo Ren" ) //this is an immutable version of the arrray, we can't add t it nor can we change it
+    println(imperials)
+
+}
