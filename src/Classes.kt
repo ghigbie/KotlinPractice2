4cd8fdea8137cf227dc67ca7939db331c5641f91
George Higbie <gehigbie@gmail.com>
2017-09-17 19:25:39 -0600

    Add acceleate method to car and truck classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -2,18 +2,29 @@
 class Car constructor(make: String, model: String){
     val make = make
     val model = model
+
+    fun accelerate(){
+        println("vroom vroom")
+    }
 }
 
-class Truck(val make: String, val model: String, val is4WD: Boolean)
+class Truck(val make: String, val model: String, val is4WD: Boolean, val towingCapacity: Int){
+    fun accelerate(){
+        println("vroom vroom")
+    }
+}
 
 fun main(args: Array<String>){
     val car = Car("Toyota", "Yaris")
     println("The car's make is: ${car.make}")
     println("The car's model is: ${car.model}")
+    car.accelerate()
 
-    val truck = Truck("Ford", "F-150", true)
+    val truck = Truck("Ford", "F-150", true, 2500)
     println("The truck's make is: ${truck.make}")
     println("The truck's model is: ${truck.model}")
     println("It is ${truck.is4WD} that the truck has a 4 wheel drive")
+    println("The truck's towing capacity is: ${truck.towingCapacity}")
+    truck.accelerate()
 
 }
