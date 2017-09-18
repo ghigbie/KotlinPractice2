32edab3869016477d1c3c001e7093ed66da52f3c
George Higbie <gehigbie@gmail.com>
2017-09-17 19:27:27 -0600

    Add done methods to car and truck

--- src/Classes.kt
+++ src/Classes.kt
@@ -6,11 +6,19 @@ class Car constructor(make: String, model: String){
     fun accelerate(){
         println("vroom vroom")
     }
+
+    fun done(){
+        println("/////////////////////")
+    }
 }
 
 class Truck(val make: String, val model: String, val is4WD: Boolean, val towingCapacity: Int){
     fun accelerate(){
         println("vroom vroom")
+    }
+
+    fun done(){
+        println("/////////////////////")
     }
 }
 
@@ -19,6 +27,7 @@ fun main(args: Array<String>){
     println("The car's make is: ${car.make}")
     println("The car's model is: ${car.model}")
     car.accelerate()
+    car.done()
 
     val truck = Truck("Ford", "F-150", true, 2500)
     println("The truck's make is: ${truck.make}")
@@ -26,5 +35,7 @@ fun main(args: Array<String>){
     println("It is ${truck.is4WD} that the truck has a 4 wheel drive")
     println("The truck's towing capacity is: ${truck.towingCapacity}")
     truck.accelerate()
+    truck.done()
+
 
 }
