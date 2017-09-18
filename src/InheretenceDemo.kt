71fc8d62b39810de7c7deeed161310b525dd5587
George Higbie <gehigbie@gmail.com>
2017-09-17 19:55:29 -0600

    Inherited from the vehicle class and add mehtods

--- src/InheretenceDemo.kt
+++ src/InheretenceDemo.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>){
 
+    val tesla = CarType("Tesla", "ModelS", 2, "Midnight Blue")
+    tesla.accelerate()
+    tesla.stop()
+    tesla.park()
+    tesla.basicInfo()
+    tesla.loadPassengers(4)
+
 }
 //using the key word open allows the class to be inherited
 open class Vehicle(val make: String, val model: String, val doors: Int){
@@ -12,9 +19,18 @@ open class Vehicle(val make: String, val model: String, val doors: Int){
         println("screech!")
     }
 
+    fun park(){
+        println("parking the vehicle")
+    }
+
     fun basicInfo(){
         println("This vehicle is a $make $model with $doors doors")
     }
 }
 
-class Car : Vehicle
+class CarType(make: String, model: String, doors: Int, var color: String ) : Vehicle(make, model, doors){
+    fun loadPassengers(numberPassengers: Int){
+        println("The $make is loading $numberPassengers passengers into the car.")
+    }
+
+}
