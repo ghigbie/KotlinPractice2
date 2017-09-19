c8bc0d348f37aed1928789ff79fe53b04b462a80
George Higbie <gehigbie@gmail.com>
2017-09-18 22:21:10 -0600

    Add aditioanl lambda expressions

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -1,3 +1,19 @@
 fun main(args: Array<String>){
 
+    val printMessage = { message: String -> println(message)}
+    //lambda expression is always surrounded by curly braces
+    //lambda expression is a function literal. it is declaired but passed immdiate;ly as an express
+    printMessage("Yo Bubba!")
+
+    val sumA = {x: Int, y: Int -> x + y}
+    println(sumA(45, 44))
+    val sumB : (Int, Int) -> Int = {x,y -> x+y}
+    println(sumB(3,7))
+
+    val divide = {x: Int, y: Int -> x/y}
+    println(divide(9,3))
+    val divide2 : (Int, Int) -> Int = {x,y -> x/y}
+    println(divide2(45, 5))
+
+
 }
