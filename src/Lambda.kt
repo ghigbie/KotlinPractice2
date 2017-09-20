e89333954f5cdae9ee7844c02912ba722f536bdb
George Higbie <gehigbie@gmail.com>
2017-09-19 19:57:38 -0600

    Edit important comments

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>){
 
     val printMessage = { message: String -> println(message)}
     //lambda expression is always surrounded by curly braces
-    //lambda expression is a function literal. it is declaired but passed immdiate;ly as an express
+    //lambda expression is a function literal. it is declared but passed immediately as an express
     printMessage("Yo Bubba!")
 
     val sumA = {x: Int, y: Int -> x + y}
@@ -14,6 +14,18 @@ fun main(args: Array<String>){
     println(divide(9,3))
     val divide2 : (Int, Int) -> Int = {x,y -> x/y}
     println(divide2(45, 5))
+
+    fun downloadData(url: String, completion: () -> Unit){
+        //sent a download request
+        //we got back data
+        //there were no network errors
+        completion()
+
+    }
+
+    downloadData("fakeUrl.com", {
+        println("This only executes after completion")
+    })
 
 
 }
