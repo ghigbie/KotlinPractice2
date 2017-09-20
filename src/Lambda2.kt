98b7c95a116eca2ff72a2dc82c84839a3e37979d
George Higbie <gehigbie@gmail.com>
2017-09-19 20:21:31 -0600

    Add new lambda expressions

--- src/Lambda2.kt
+++ src/Lambda2.kt
@@ -9,4 +9,29 @@ fun main(args: Array<String>){
     val returnStuff = {stuff: String -> stuff}
     println(returnStuff("Bubba Patty"))
 
+    val sumA = {a: Int, b: Int, c: Int -> a+b+c}
+    println(sumA(5, 4, 3))
+
+    val multiplyA = {a: Int, b: Int, c: Int -> a*b*c }
+    println(multiplyA(5, 4, 3))
+
+    val divideA = {a: Int, b: Int -> a/b}
+    println(divideA(12,3))
+
+    val concatString : (String, String) -> String = {a,b -> "$a $b"}
+    println(concatString("The", "dog"))
+    val concatStrings : (String, String, String) -> String = {a,b,c -> "$a $b $c"}
+    println(concatStrings("The", "dog", "runs"))
+    val writeSentence : (String, String, String, String) -> String = {a,b,c,d -> "$a $b $c $d"}
+    println(writeSentence("The", "dog", "runs", "away"))
+
+    val sumB : (Int, Int) -> String = {a, b -> (a+b).toString()}
+    println(sumB(4, 4))
+
+    val multiplyB : (Int, Int) -> String = {a,b -> (a/b).toString()}
+    println(multiplyB(8, 4))
+
+
+
+
 }
